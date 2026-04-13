e813edb624417f1f0f94dadcfc9191efe8502da8
vegasword <66827361+vegasword@users.noreply.github.com>
2026-04-13 21:54:12 +0200

    Port was cursed

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -9,8 +9,7 @@ import io.ktor.server.plugins.contentnegotiation.*
 fun main(args: Array<String>) {
     embeddedServer(
         Netty,
-        port = 31337,
-        host = "0.0.0.0",
+        port = 8080,
         module = Application::module
     ).start(wait = true)
 }
