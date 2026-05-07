518ba56bff2c6b8549208c45b0e231ef30e70ce1
lotratemi <138588997+Lotratemi@users.noreply.github.com>
2026-05-07 20:06:57 +0200

    Remove streak references

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,12 +3,10 @@ package com.codingfactory
 import com.codingfactory.repositories.ConversationRepository
 import com.codingfactory.repositories.ObjectiveRepository
 import com.codingfactory.repositories.ProfilingRepository
-import com.codingfactory.repositories.StreakRepository
 import com.codingfactory.repositories.UserRepository
 import com.codingfactory.services.CoachService
 import com.codingfactory.services.ObjectiveService
 import com.codingfactory.services.ProfilingService
-import com.codingfactory.services.StreakService
 import com.codingfactory.services.UserService
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
@@ -30,5 +28,5 @@ fun Application.module() {
         ProfilingRepository(), mistralClient, mistralApiUrl, mistralApiKey, mistralAiModel
     )
 
-    configureRouting(userService, objectiveService, streakService, coachService, profilingService)
+    configureRouting(userService, objectiveService, coachService, profilingService)
 }
